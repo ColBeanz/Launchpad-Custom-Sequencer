fd8628dbf58aaef9c9704efb510b9d246ad0d91f
cecil-chamberlain <not.my@realaddress.com>
2018-04-01 13:06:36 +0100

    Implements Playhead

--- src/app.c
+++ src/app.c
@@ -1,22 +1,22 @@
 /******************************************************************************
- 
+
  Copyright (c) 2015, Focusrite Audio Engineering Ltd.
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,7 +27,7 @@
  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  *****************************************************************************/
 
 //______________________________________________________________________________
@@ -46,6 +46,10 @@
 // and recall the pad state from flash.
 //______________________________________________________________________________
 
+// timing defs
+#define MS_PER_MIN 60000
+#define CLOCK_RATE 24
+
 // store ADC frame pointer
 static const u16 *g_ADC = 0;
 
@@ -53,6 +57,66 @@ static const u16 *g_ADC = 0;
 #define BUTTON_COUNT 100
 
 u8 g_Buttons[BUTTON_COUNT] = {0};
+
+#define RANGE 8
+#define STEPS 8
+
+static u8 DRUM_NOTES[RANGE] =
+{
+	36, 37, 38, 39, 40, 41, 42, 43, 44
+};
+
+struct PadState{
+	u8 steps[STEPS];
+	u8 pos;
+	u8 notes[RANGE];
+};
+
+struct PadState Channel1 = { .pos = 0};
+
+int IsNoteOn(u8 flags, u8 bit)
+{
+    return ((flags & (1 << bit)) != 0);
+}
+
+void CalclulateNotes(u8* notes, u8 step) // step is the 8 bit binary flag from which the midi notes will be derived
+{
+    for (int i = 1; i <= RANGE; i++)
+    {
+        if (IsNoteOn(step, i))
+        {
+            notes[i -1] = DRUM_NOTES[i -1];
+        }
+    }
+};
+
+int CalculateMsPerClock(int tempo)
+{
+    return MS_PER_MIN / (CLOCK_RATE * tempo);
+}
+
+void PlotPlayhead(int step)
+{
+	if (step == 0)
+	{
+		for (int i = 1; i < 9; i++)
+		{
+			hal_plot_led(TYPEPAD, 8 + (i * 10), 0, 0, 0);
+		}
+	}
+	else
+	{
+		for (int i = 1; i < 9; i++)
+		{
+			hal_plot_led(TYPEPAD, step + (i * 10), 0, 0, 0);
+		}
+	}
+
+	for (int i = 1; i < 9; i++)
+	{
+		hal_plot_led(TYPEPAD, (step + 1) + (i * 10), MAXLED, MAXLED, MAXLED);
+	}
+}
 
 //______________________________________________________________________________
 
@@ -67,17 +131,18 @@ void app_surface_event(u8 type, u8 index, u8 value)
             {
                 g_Buttons[index] = MAXLED * !g_Buttons[index];
             }
-            
+
+
             // example - light / extinguish pad LEDs
             hal_plot_led(TYPEPAD, index, 0, 0, g_Buttons[index]);
-            
+
             // example - send MIDI
             hal_send_midi(DINMIDI, NOTEON | 0, index, value);
-	    hal_send_midi(USBSTANDALONE, NOTEONCHONE, index, value);
-            
+			hal_send_midi(USBSTANDALONE, NOTEONCHONE, index, value);
+
         }
         break;
-            
+
         case TYPESETUP:
         {
             if (value)
@@ -99,7 +164,7 @@ void app_midi_event(u8 port, u8 status, u8 d1, u8 d2)
     {
         hal_send_midi(DINMIDI, status, d1, d2);
     }
-    
+
     // // example -MIDI interface functionality for DIN -> USB "MIDI" port port
     if (port == DINMIDI)
     {
@@ -120,8 +185,8 @@ void app_aftertouch_event(u8 index, u8 value)
 {
     // example - send poly aftertouch to MIDI ports
     hal_send_midi(USBMIDI, POLYAFTERTOUCH | 0, index, value);
-    
-    
+
+
 }
 
 //______________________________________________________________________________
@@ -141,21 +206,43 @@ void app_cable_event(u8 type, u8 value)
 
 //______________________________________________________________________________
 
+
+static int g_tempo = 120;
+static int g_ms_per_tick;
+
+
 void app_timer_event()
 {
-    // example - send MIDI clock at 125bpm
-#define TICK_MS 20
-    
-    static u8 ms = TICK_MS;
-    
-    if (++ms >= TICK_MS)
+    static int ms = 0;
+    static int semiquaver = 0;
+
+    if (++ms >= g_ms_per_tick)
     {
         ms = 0;
-        
+
         // send a clock pulse up the USB
         hal_send_midi(USBSTANDALONE, MIDITIMINGCLOCK, 0, 0);
+
+        if (++semiquaver >= 6)
+        {
+            semiquaver = 0;
+
+		    if (Channel1.pos >= STEPS)
+		    {
+				Channel1.pos = 0;
+		    }
+
+			PlotPlayhead(Channel1.pos);
+
+		    Channel1.pos++;
+		}
+
     }
-    
+
+    static int position = 0;
+
+
+
 /*
 	// alternative example - show raw ADC data as LEDs
 	for (int i=0; i < PAD_COUNT; ++i)
@@ -165,9 +252,9 @@ void app_timer_event()
 		u16 r = 0;
 		u16 g = 0;
 		u16 b = 0;
-		
+
 		u16 x = (3 * MAXLED * g_ADC[i]) >> 12;
-		
+
 		if (x < MAXLED)
 		{
 			r = x;
@@ -182,7 +269,7 @@ void app_timer_event()
 			g = MAXLED - x;
 			b = x - MAXLED;
 		}
-		
+
 		hal_plot_led(TYPEPAD, ADC_MAP[i], r, g, b);
 	}
  */
@@ -192,20 +279,22 @@ void app_timer_event()
 
 void app_init(const u16 *adc_raw)
 {
+    // calculate how many milliseconds per tick for midi clock
+    g_ms_per_tick = CalculateMsPerClock(g_tempo);
     // example - load button states from flash
     hal_read_flash(0, g_Buttons, BUTTON_COUNT);
-    
+
     // example - light the LEDs to say hello!
     for (int i=0; i < 10; ++i)
     {
         for (int j=0; j < 10; ++j)
         {
             u8 b = g_Buttons[j*10 + i];
-            
+
             hal_plot_led(TYPEPAD, j*10 + i, 0, 0, b);
         }
     }
-	
+
 	// store off the raw ADC frame pointer for later use
 	g_ADC = adc_raw;
 }
